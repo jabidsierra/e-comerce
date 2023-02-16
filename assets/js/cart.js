b637b3b35fef3d680fe0bc8419cc02ac0c4a3791
jabidsierra <67206226+jabidsierra@users.noreply.github.com>
2023-02-16 16:07:35 -0500

    Update cart.js

--- assets/js/cart.js
+++ assets/js/cart.js
@@ -81,11 +81,10 @@ const db = [
 const products = db
 // #2 Pintar los productos en el DOM
 const productContainer = document.getElementById("products__content");
-function printProducts() {
+function displayProducts() {
   let html = "";
   for (const product of products) {
-    html += `
-    <article class="products__card ${products.category}">
+    html += `<article class="products__card ${product.category}">
       <div class="products__shape">
         <img src="${product.image}" alt="${product.name}" class="products__img">
       </div>
